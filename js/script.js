b64a6dfab1db764ab7acb95215d1b2aab3a37d74
anthonbrooks <51774498+anthonbrooks@users.noreply.github.com>
2026-01-17 14:26:15 -0700

    Add data-key to pads

--- js/script.js
+++ js/script.js
@@ -2,4 +2,6 @@ window.addEventListener('keydown', (e) => {
     const audio = document.querySelector(`audio[data-key=${e.code}]`);
     console.log(audio);
     if (!audio) return; // return if no audio associated with key
+    audio.currentTime = 0; // rewind audio for successive key downs
+    audio.play();
 });
